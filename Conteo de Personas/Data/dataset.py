e5a3474ab5fafb3955bff97515daa226042b474b
Hector David <hectordavid55555211@gmail.com>
2026-07-15 17:45:59 -0400

    commit

--- Conteo de Personas/Data/dataset.py
+++ Conteo de Personas/Data/dataset.py
@@ -1,28 +1,3 @@
-"""
-dataset.py
-==============================
-
-Carga el dataset para un detector tipo YOLO.
-
-Estructura esperada:
-
-dataset/
-│
-├── train/
-│   ├── images/
-│   └── labels/
-│
-├── valid/
-│   ├── images/
-│   └── labels/
-│
-└── test/
-    ├── images/
-    └── labels/
-
-Cada imagen debe tener un txt con el mismo nombre.
-"""
-
 from Data.parser import Transforms
 from pathlib import Path
 import tensorflow as tf
@@ -31,182 +6,87 @@ import tensorflow as tf
 class PeopleDataset:
 
     def __init__(self, config):
-
+        "Constructor, la configuracion inicial es la de config.py"
         self.image_size = config["IMAGE_SIZE"]
         self.batch_size = config["BATCH_SIZE"]
         self.shuffle = config.get("SHUFFLE", True)
         self.buffer_size = config.get("BUFFER_SIZE", 1000)
         self.autotune = tf.data.AUTOTUNE
-
-    ####################################################################
-    # Leer imagen
-    ####################################################################
+        self.transforms = Transforms(image_size=self.image_size)
 
     def load_image(self, image_path):
+        "Cargas las imagenes"
 
-        image = tf.io.read_file(image_path)
-
-        image = tf.image.decode_jpeg(
-            image,
-            channels=3
-        )
-
-        image = tf.image.resize(
-            image,
-            (self.image_size,
-             self.image_size)
-        )
-
-        image = tf.cast(image, tf.float32)
-
+        image = tf.io.read_file(image_path)#Lee las imagenes
+        image = tf.image.decode_jpeg(image,channels=3)#decode rgb
+        image = tf.image.resize(image,(self.image_size,self.image_size))#Las redimensiona segun las configuraciones
+        image = tf.cast(image, tf.float32)#Las castea
         image /= 255.0
 
         return image
 
-    ####################################################################
-    # Leer etiquetas YOLO
-    ####################################################################
-
     def load_label(self, label_path):
-
-        text = tf.io.read_file(label_path)
-
+        "Procesa las etiquetas de las imagenes"
+        text = tf.io.read_file(label_path)#Lee las etiquetas y las guarda
         text = tf.strings.strip(text)
-
-        lines = tf.strings.split(text, "\n")
-
+        lines = tf.strings.split(text, "\n")#Las divide
         values = tf.strings.split(lines)
-
-        values = tf.strings.to_number(
-            values,
-            tf.float32
-        )
+        values = tf.strings.to_number(values,tf.float32)
 
         return values
 
-    ####################################################################
-    # Leer una muestra completa
-    ####################################################################
-
-    def parse_sample(self,
-                     image_path,
-                     label_path):
-        
+    def parse_sample(self,image_path,label_path):
+        "Toma una de ejemplo"
         image = self.load_image(image_path)
-
         boxes = self.load_label(label_path)
-
-        image, boxes = self.transforms(
-            image,
-            boxes
-        )
+        image, boxes = self.transforms(image,boxes)
 
         return image, boxes
 
-    ####################################################################
-    # Obtener rutas
-    ####################################################################
-
     def get_paths(self, dataset_path, split):
-
+        "Obtiene rutas"
         dataset_path = Path(dataset_path)
-
         image_dir = dataset_path / split / "images"
-
         label_dir = dataset_path / split / "labels"
-
         image_paths = sorted(image_dir.glob("*"))
-
         label_paths = []
 
         for image in image_paths:
-
             label = label_dir / (image.stem + ".txt")
-
             label_paths.append(label)
 
         return image_paths, label_paths
 
-    ####################################################################
-    # Crear Dataset TensorFlow
-    ####################################################################
+    def create_dataset(self,dataset_path,split):
+        "Crea el dataset de tensorflow"
+        image_paths, label_paths = self.get_paths(dataset_path,split)
 
-    def create_dataset(self,
-                       dataset_path,
-                       split):
-
-        image_paths, label_paths = self.get_paths(
-            dataset_path,
-            split
-        )
-
+        #obtiene las ubicaciones
         image_paths = [str(x) for x in image_paths]
         label_paths = [str(x) for x in label_paths]
 
-        dataset = tf.data.Dataset.from_tensor_slices(
-            (
-                image_paths,
-                label_paths
-            )
-        )
+        dataset = tf.data.Dataset.from_tensor_slices((image_paths,label_paths))
 
         if split == "train" and self.shuffle:
-
-            dataset = dataset.shuffle(
-                self.buffer_size
-            )
+            dataset = dataset.shuffle(self.buffer_size)
 
         dataset = dataset.map(
             self.parse_sample,
             num_parallel_calls=self.autotune
         )
 
-        # IMPORTANTE:
-        # Como cada imagen puede tener distinta cantidad de personas,
-        # usamos padded_batch.
-
         dataset = dataset.padded_batch(
-
             self.batch_size,
-
-            padded_shapes=(
-
-                [self.image_size,
-                 self.image_size,
-                 3],
-
-                [None, 5]
-
-            ),
-
-            padding_values=(
-
-                tf.constant(
-                    0,
-                    tf.float32
-                ),
-
-                tf.constant(
-                    -1,
-                    tf.float32
-                )
-
-            )
-
+            padded_shapes=([self.image_size,self.image_size,3],[None, 5]),
+            padding_values=(tf.constant(0,tf.float32),tf.constant(-1,tf.float32))
         )
 
-        dataset = dataset.prefetch(
-            self.autotune
-        )
+        dataset = dataset.prefetch(self.autotune)
 
         return dataset
 
-    ####################################################################
-    # Cargar train, valid y test
-    ####################################################################
-
     def load(self, dataset_path):
-
+        "Carga el train, valid y test"
         train = self.create_dataset(
             dataset_path,
             "train"
--- Conteo de Personas/Data/visualization.py
+++ Conteo de Personas/Data/visualization.py
@@ -1,11 +1,3 @@
-"""
-visualize.py
-======================================
-
-Funciones para visualizar imágenes y
-bounding boxes del detector.
-"""
-
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import tensorflow as tf
@@ -13,49 +5,33 @@ import numpy as np
 
 
 class Visualizer:
-
+    "El objetivo es comprobar visualmente que las imagenes concuerden con las cajas de las etiquetas"
     def __init__(self, class_names=None):
-
         if class_names is None:
             class_names = ["person"]
-
         self.class_names = class_names
 
-    ###########################################################
-    # Convertir coordenadas YOLO -> XYXY
-    ###########################################################
-
     def yolo_to_xyxy(self, box, image_width, image_height):
-
+        "Conversion de coordenadas"
         cls, xc, yc, w, h = box
-
         xc *= image_width
         yc *= image_height
-
         w *= image_width
         h *= image_height
-
         xmin = xc - w / 2
         ymin = yc - h / 2
-
         xmax = xc + w / 2
         ymax = yc + h / 2
 
         return xmin, ymin, xmax, ymax, int(cls)
 
-    ###########################################################
-    # Dibujar una imagen
-    ###########################################################
-
     def show(self, image, boxes):
-
+        "Las muestra"
         if tf.is_tensor(image):
             image = image.numpy()
 
         h, w = image.shape[:2]
-
         fig, ax = plt.subplots(figsize=(8,8))
-
         ax.imshow(image)
 
         for box in boxes:
@@ -64,11 +40,7 @@ class Visualizer:
             if box[0] < 0:
                 continue
 
-            xmin, ymin, xmax, ymax, cls = self.yolo_to_xyxy(
-                box,
-                w,
-                h
-            )
+            xmin, ymin, xmax, ymax, cls = self.yolo_to_xyxy(box,w,h)
 
             rect = patches.Rectangle(
                 (xmin, ymin),
@@ -93,22 +65,10 @@ class Visualizer:
         plt.axis("off")
         plt.show()
 
-    ###########################################################
-    # Visualizar un batch
-    ###########################################################
-
-    def show_batch(self,
-                   images,
-                   boxes,
-                   max_images=4):
-
+    def show_batch(self,images,boxes,max_images=4):
+        "Muestra los batches"
         n = min(max_images, images.shape[0])
-
-        fig, axes = plt.subplots(
-            1,
-            n,
-            figsize=(5*n,5)
-        )
+        fig, axes = plt.subplots(1,n,figsize=(5*n,5))
 
         if n == 1:
             axes = [axes]
@@ -116,11 +76,8 @@ class Visualizer:
         for i in range(n):
 
             image = images[i].numpy()
-
             ax = axes[i]
-
             ax.imshow(image)
-
             h, w = image.shape[:2]
 
             for box in boxes[i]:
@@ -128,11 +85,7 @@ class Visualizer:
                 if box[0] < 0:
                     continue
 
-                xmin, ymin, xmax, ymax, cls = self.yolo_to_xyxy(
-                    box.numpy(),
-                    w,
-                    h
-                )
+                xmin, ymin, xmax, ymax, cls = self.yolo_to_xyxy(box.numpy(),w,h)
 
                 rect = patches.Rectangle(
                     (xmin, ymin),
@@ -160,32 +113,18 @@ class Visualizer:
 
         plt.show()
 
-    ###########################################################
-    # Mostrar predicciones
-    ###########################################################
-
-    def show_predictions(
-            self,
-            image,
-            boxes,
-            scores):
+    def show_predictions(self,image,boxes,scores):
 
         if tf.is_tensor(image):
             image = image.numpy()
 
         h, w = image.shape[:2]
-
         fig, ax = plt.subplots(figsize=(8,8))
-
         ax.imshow(image)
 
         for box, score in zip(boxes, scores):
 
-            xmin, ymin, xmax, ymax, cls = self.yolo_to_xyxy(
-                box,
-                w,
-                h
-            )
+            xmin, ymin, xmax, ymax, cls = self.yolo_to_xyxy(box,w,h)
 
             rect = patches.Rectangle(
                 (xmin,ymin),
@@ -197,7 +136,6 @@ class Visualizer:
             )
 
             ax.add_patch(rect)
-
             texto = f"{self.class_names[cls]} {score:.2f}"
 
             ax.text(
